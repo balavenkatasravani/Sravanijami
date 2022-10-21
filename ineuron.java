c4eabda971fbb82e802b77509a1311da82c5b3e7
balavenkatasravani <balaee218@gmail.com>
2022-10-21 09:10:27 +0530

    sharing assingment files to mentor

---
+++ ineuron.java
@@ -0,0 +1,128 @@
+package HelloWorld;
+
+public class ineuron {
+	public static void main(String[] args) {
+		//1.Write a program(WAP) to print INEURON using pattern programming logic.
+		int n=5;
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++)
+			   {  
+					if(i==2)
+				
+					System.out.print(" ");
+				else
+					System.out.print(" ");
+		       }
+				for(int j=0;j<n;j++)
+		   {  
+				if(i==0||i==4||j==2)
+			
+				System.out.print("i");
+			else
+				System.out.print(" ");
+	       }
+				for(int j=0;j<n;j++)
+				   {  
+						if(i==2)
+					
+						System.out.print(" ");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+					if(j==0||j==4||i==j)
+					
+						System.out.print("n");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+						if(i==2)
+					
+						System.out.print(" ");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+					if(i==0||i==2||i==4||j==0)
+					
+						System.out.print("e");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+						if(i==2)
+					
+						System.out.print(" ");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+					
+						if((j==0&&i!=0&&i!=4)||(i==4&&j!=0&&j!=4)||(j==4&&i!=0&&i!=4)||(i==0&&j!=1&&j!=2&&j!=3))
+						System.out.print("u");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+						if(i==2)
+					
+						System.out.print(" ");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+					if((i==0&&j!=4)||(i==2&&j!=4)||(j==0)||(i==1&&j==4)||(i==3&&j==3)||(i==4&&j==4))
+
+					
+						System.out.print("r");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+						if(i==2)
+					
+						System.out.print(" ");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+					
+					if((j==0&&i!=0&&i!=4)||(i==4&&j!=0&&j!=4)||(j==4&&i!=0&&i!=4)||(i==0&&j!=0&&j!=4))
+						System.out.print("o");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+						if(i==2)
+					
+						System.out.print(" ");
+					else
+						System.out.print(" ");
+			       }
+				for(int j=0;j<n;j++)
+				   {  
+					if(j==0||j==4||i==j)
+					
+						System.out.print("n");
+					else
+						System.out.print(" ");
+			       }
+		System.out.println();
+		
+	}
+
+			}
+	}
+
+
